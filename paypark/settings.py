a70ed2e92fcebce0c12c9973d12397016e297f0f
Muhammad Fuhad MK <fuhadmk4@gmail.com>
2025-01-21 09:39:10 +0530

    settig change

--- paypark/settings.py
+++ paypark/settings.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field  gbbebejhbgergr
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
