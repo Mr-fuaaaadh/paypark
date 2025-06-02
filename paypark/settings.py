7a1ef22b950b0820eb7ecb959a3492bcd27b70bf
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-06-02 14:48:22 +0530

    rbnvjrvrv

--- paypark/settings.py
+++ paypark/settings.py
@@ -93,23 +93,23 @@ WSGI_APPLICATION = 'paypark.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'paypark_db',   
-        'USER': 'postgres',        
-        'PASSWORD': 'paypark123!!@@',   
-        'HOST': 'localhost',     
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}           
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'paypark_db',   
+#         'USER': 'postgres',        
+#         'PASSWORD': 'paypark123!!@@',   
+#         'HOST': 'localhost',     
+#         'PORT': '5432',
+#     }
+# }           
 
 
 # Password validation
